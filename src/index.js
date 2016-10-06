cd638f05ab65d665a6c849d2be269755f8d1098c
nguyenanhvu <nguyenanhvu.cs@gmail.com>
2016-10-06 16:49:48 +0700

    first

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';	
+// import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap-theme.css';
+// import DataTable from 'react-data-components';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+
+
+
+
+
+
+
+  // ReactDOM.render(<DataTable />, document.getElementById('root'));
+
